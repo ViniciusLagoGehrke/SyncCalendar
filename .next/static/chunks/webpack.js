60da319e39a3ef4fbee48e34d6fb6b12068e77ea
Vinicius Lago Gehrke <vinictus@gmail.com>
2021-03-16 10:21:57 +0000

    added _error.js

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4a569facff3898b66a69";
+/******/ 	var hotCurrentHash = "393cb6659f6dc22d2248";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
