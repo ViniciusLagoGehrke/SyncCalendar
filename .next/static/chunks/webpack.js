9b7cc075f571b6094d4038b092b00345b2939d62
Vinicius Lago Gehrke <vinictus@gmail.com>
2021-03-24 02:09:45 +0000

    initiating CRUD on users list

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1dbf7cef43c5d6e9d563";
+/******/ 	var hotCurrentHash = "9f2ec500ceb09bd053a0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
